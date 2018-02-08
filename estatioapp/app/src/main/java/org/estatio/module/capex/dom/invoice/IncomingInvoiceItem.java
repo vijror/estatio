501d1e0e7c50c30555ee06eb453d5fe991f401b9
Johan Doornenbal <johan@filternet.nl>
2018-02-08 15:07:11 +0100

    ECP-438: prepares moving of order items and/or incoming invoice items to other project

--- estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/IncomingInvoiceItem.java
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/invoice/IncomingInvoiceItem.java
@@ -37,6 +37,9 @@ import org.apache.isis.applib.services.repository.RepositoryService;
 
 import org.incode.module.base.dom.valuetypes.LocalDateInterval;
 
+import org.estatio.module.asset.dom.FixedAsset;
+import org.estatio.module.base.platform.applib.ReasonBuffer2;
+import org.estatio.module.budget.dom.budgetitem.BudgetItem;
 import org.estatio.module.capex.dom.documents.BudgetItemChooser;
 import org.estatio.module.capex.dom.invoice.approval.IncomingInvoiceApprovalState;
 import org.estatio.module.capex.dom.items.FinancialItem;
@@ -46,16 +49,13 @@ import org.estatio.module.capex.dom.orderinvoice.OrderItemInvoiceItemLink;
 import org.estatio.module.capex.dom.orderinvoice.OrderItemInvoiceItemLinkRepository;
 import org.estatio.module.capex.dom.project.Project;
 import org.estatio.module.capex.dom.project.ProjectRepository;
+import org.estatio.module.capex.dom.util.FinancialAmountUtil;
 import org.estatio.module.capex.dom.util.PeriodUtil;
-import org.estatio.module.asset.dom.FixedAsset;
-import org.estatio.module.budget.dom.budgetitem.BudgetItem;
 import org.estatio.module.charge.dom.Applicability;
 import org.estatio.module.charge.dom.Charge;
 import org.estatio.module.charge.dom.ChargeRepository;
 import org.estatio.module.invoice.dom.Invoice;
 import org.estatio.module.invoice.dom.InvoiceItem;
-import org.estatio.module.capex.dom.util.FinancialAmountUtil;
-import org.estatio.module.base.platform.applib.ReasonBuffer2;
 import org.estatio.module.tax.dom.Tax;
 import org.estatio.module.tax.dom.TaxRate;
 
@@ -258,6 +258,10 @@ public class IncomingInvoiceItem extends InvoiceItem<IncomingInvoice,IncomingInv
 
     public void setProject(final Project project) {
         this.project = invalidateApprovalIfDiffer(this.project, project);
+    }
+
+    public void setProjectByPassingInvalidateApproval(final Project project){
+        this.project = project;
     }
 
 
