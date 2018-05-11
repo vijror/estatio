ad04db580a5f489e89b22a0f5ccf4ff63cf922e4
Johan Doornenbal <johan@filternet.nl>
2018-05-11 11:07:32 +0200

    EST-1830: turnover rent import w.i.p. - todo integ test

--- estatioapp/app/src/main/java/org/estatio/module/lease/imports/LeaseTermForTurnOverRentFixedImport.java
+++ estatioapp/app/src/main/java/org/estatio/module/lease/imports/LeaseTermForTurnOverRentFixedImport.java
@@ -3,6 +3,8 @@ package org.estatio.module.lease.imports;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import com.google.common.collect.Lists;
 
 import org.joda.time.LocalDate;
@@ -13,11 +15,18 @@ import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.Nature;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.applib.services.message.MessageService;
 
 import org.isisaddons.module.excel.dom.ExcelFixture;
 import org.isisaddons.module.excel.dom.ExcelFixtureRowHandler;
 
 import org.estatio.module.base.dom.Importable;
+import org.estatio.module.lease.dom.Lease;
+import org.estatio.module.lease.dom.LeaseItem;
+import org.estatio.module.lease.dom.LeaseItemType;
+import org.estatio.module.lease.dom.LeaseRepository;
+import org.estatio.module.lease.dom.LeaseTermForFixed;
+import org.estatio.module.lease.dom.LeaseTermRepository;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,7 +35,7 @@ import lombok.Setter;
         nature = Nature.VIEW_MODEL,
         objectType = "org.estatio.dom.viewmodels.LeaseTermForTurnOverRentFixedImport"
 )
-public class LeaseTermForTurnOverRentFixedImport extends LeaseTermImportAbstract implements ExcelFixtureRowHandler, Importable {
+public class LeaseTermForTurnOverRentFixedImport implements ExcelFixtureRowHandler, Importable {
 
     private static final Logger LOG = LoggerFactory.getLogger(LeaseTermForTurnOverRentFixedImport.class);
 
@@ -52,6 +61,9 @@ public class LeaseTermForTurnOverRentFixedImport extends LeaseTermImportAbstract
     private LocalDate startDate;
 
     @Getter @Setter
+    private LocalDate endDate;
+
+    @Getter @Setter
     private BigDecimal value;
 
     @Programmatic
@@ -67,8 +79,45 @@ public class LeaseTermForTurnOverRentFixedImport extends LeaseTermImportAbstract
     @Programmatic
     @Override
     public List<Object> importData(Object previousRow) {
+        Lease leaseToUpdate = leaseRepository.findLeaseByReference(getLeaseReference());
+        if (leaseToUpdate == null) {
+            messageService.warnUser(String.format("Lease with reference %s not found", getLeaseReference()));
+            return Lists.newArrayList();
+        }
+        LeaseItem itemToUpdate = leaseToUpdate.findFirstItemOfType(LeaseItemType.TURNOVER_RENT_FIXED);
+        if (itemToUpdate == null) {
+            messageService.warnUser(String.format("No lease item of type TURNOVER_RENT_FIXED found on lease with reference %s", getLeaseReference()));
+            return Lists.newArrayList();
+        }
+        if (getValuePrevious()!=null && getStartDatePrevious() !=null) {
+            updateOrCreateTerm(itemToUpdate, getStartDatePrevious(), null, getValuePrevious());
+        }
+        if (getValueCurrent()!=null && getStartDateCurrent() !=null) {
+            updateOrCreateTerm(itemToUpdate, getStartDateCurrent(), null, getValueCurrent());
+        }
+        if (getValue()!=null && getStartDate() !=null) {
+            updateOrCreateTerm(itemToUpdate, getStartDate(), getEndDate(), getValue());
+        }
 
         return Lists.newArrayList();
-
     }
+
+    void updateOrCreateTerm(final LeaseItem itemToUpdate, final LocalDate startDate, final LocalDate endDate, final BigDecimal value) {
+        LeaseTermForFixed termToUpdate = (LeaseTermForFixed) leaseTermRepository.findByLeaseItemAndStartDate(itemToUpdate, startDate);
+        if (termToUpdate!=null){
+            termToUpdate.setValue(value);
+            termToUpdate.setEndDate(endDate!=null ? endDate : startDate.plusYears(1).minusDays(1));
+        } else {
+            LeaseTermForFixed newTerm = (LeaseTermForFixed) itemToUpdate.newTerm(startDate, endDate!=null ? endDate : startDate.plusYears(1).minusDays(1));
+            newTerm.setValue(value);
+        }
+    }
+
+    @Inject
+    LeaseRepository leaseRepository;
+
+    @Inject
+    LeaseTermRepository leaseTermRepository;
+
+    @Inject MessageService messageService;
 }
