e7400adef9888e3298f56973cb30943a0fa25abd
danhaywood <dan@haywood-associates.co.uk>
2019-02-09 13:06:43 +0000

    ECP-927: renames OrderAttribute#order to 'ordr' to avoid JDOQL keyword clash

--- estatioapp/app/src/main/java/org/estatio/module/capex/dom/order/Order.java
+++ estatioapp/app/src/main/java/org/estatio/module/capex/dom/order/Order.java
@@ -1129,7 +1129,7 @@ public class Order extends UdoDomainObject2<Order> implements Stateful {
         return this;
     }
 
-    @Persistent(mappedBy = "order", dependentElement = "false")
+    @Persistent(mappedBy = "ordr", dependentElement = "false")
     @Getter @Setter
     private SortedSet<OrderAttribute> attributes = new TreeSet<OrderAttribute>();
 
